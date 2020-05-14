6dff4e3456f82224136c1851c3b1edb4bc3de965
Taylor Piccarreto <tpic12@gmail.com>
2020-05-14 12:33:36 -0400

    cors options

--- src/app.js
+++ src/app.js
@@ -20,7 +20,12 @@ app.use(
     skip: () => NODE_ENV === "test",
   })
 );
-app.use(cors());
+
+const corsOptions = {
+  origin: "https://linus-ten.now.sh/",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(cors(corsOptions));
 app.use(helemt());
 
 app.use("/cabinet", cabinetRouter);
